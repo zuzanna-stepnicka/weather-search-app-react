4d515c7322f586efee4f19c1cc2d2ee0de88eb20
zuzanna-stepnicka <zuzanna.stepnicka@gmail.com>
2022-10-16 11:09:20 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
+      <div className="container1">
+        <h1>Weather App</h1>
+        <Weather />
+      </div>
     </div>
   );
 }
